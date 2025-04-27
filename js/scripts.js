66e6957cc4097005e607a232f2dc85e8cc5eec76
Dorian <117446752+DorianTrazos@users.noreply.github.com>
2025-04-27 11:16:55 +0200

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,25 @@ const totalCards = 12;
 
 let pokeNumbers = [];
 
+let allCards = null;
+let cardA = null;
+let cardB = null;
+
+let canPlay = false;
+
 const getRandomNumber = (max = 150) => Math.floor(Math.random() * max + 1);
+
+const showAllCards = () => {
+  allCards.forEach((card, index) => {
+    if (!card.dataset.win) {
+      card.classList.add('card-show');
+      const pokeImage = `url(../assets/images/${pokeNumbers[index]}.png)`;
+      card.style.setProperty('--poke-image', pokeImage);
+    }
+  });
+
+  setTimeout(hideCards, 4000);
+};
 
 const createGameBoard = () => {
   gameContainerElement.textContent = '';
@@ -15,15 +33,15 @@ const createGameBoard = () => {
     const cardFront = document.createElement('div');
     cardFront.classList.add('card-front');
     card.append(cardFront);
-    const pokeImage = `url(../assets/images/${pokeNumbers[i]}.png)`;
-    cardFront.style.setProperty('--poke-image', pokeImage);
-    // const cardBack = document.createElement('div');
-    // cardBack.classList.add('card-back');
-    // card.append(cardBack);
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('card-back');
+    card.append(cardBack);
     fragment.append(card);
   }
 
   gameContainerElement.append(fragment);
+  allCards = gameContainerElement.querySelectorAll('.card');
+  setTimeout(showAllCards, 1000);
 };
 
 const getPokemonsToPlay = () => {
@@ -39,12 +57,54 @@ const getPokemonsToPlay = () => {
   createGameBoard();
 };
 
-getPokemonsToPlay();
+const hideCards = () => {
+  allCards.forEach(card => {
+    if (!card.dataset.win) {
+      card.classList.remove('card-show');
+      card.dataset.pokeImage = '';
+      card.dataset.pokeImage = '';
+      card.style.setProperty('--poke-image', '');
+      card.style.setProperty('--poke-image', '');
+    }
+  });
 
-gameContainerElement.addEventListener('click', event => {
+  cardA = null;
+  cardB = null;
+  canPlay = true;
+};
+
+const checkCardsSelected = card => {
+  if (card.dataset.win) return;
+  if (!cardA) cardA = card;
+  else if (!cardB) cardB = card;
+  if (!cardA || !cardB) return;
+  const pokeNumberA = cardA.dataset.pokeImage;
+  const pokeNumberB = cardB.dataset.pokeImage;
+  canPlay = false;
+  if (pokeNumberA === pokeNumberB) {
+    console.log('CORRECT');
+    cardA.dataset.win = true;
+    cardB.dataset.win = true;
+  } else {
+    console.log('NOP');
+  }
+  setTimeout(hideCards, 500);
+};
+
+const showCard = event => {
   const card = event.target.closest('.card'); // Buscar el ancestro que tenga la clase 'card'
-  if (!card) return; // Si no clicaste en un .card, nos vamos
+  if (!card || !canPlay) return; // Si no clicaste en un .card, nos vamos
+
+  card.classList.add('card-show');
 
   const index = [...gameContainerElement.children].indexOf(card);
-  console.log(index);
-});
+  const pokeImage = `url(../assets/images/${pokeNumbers[index]}.png)`;
+  card.style.setProperty('--poke-image', pokeImage);
+  card.dataset.pokeImage = pokeNumbers[index];
+
+  checkCardsSelected(card);
+};
+
+getPokemonsToPlay();
+
+gameContainerElement.addEventListener('click', showCard);
